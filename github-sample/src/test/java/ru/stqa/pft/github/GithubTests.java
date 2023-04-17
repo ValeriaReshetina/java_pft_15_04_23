2809ce32f753691fa040d14d3e6f646204462375
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-04-17 20:24:29 +0300

    commit for checking

--- github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -1,14 +1,18 @@
 package ru.stqa.pft.github;
 
-import com.jcabi.github.Github;
-import com.jcabi.github.RtGithub;
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
 import org.testng.annotations.Test;
 
 public class GithubTests {
 
     @Test
     public void testCommits() {
-        Github github = new RtGithub("");
-
+        Github github = new RtGithub("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");
+        RepoCommits commits = github.repos()
+                .get(new Coordinates.Simple("Valeria Reshetina", "java_pft_15_04_23")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(commit);
+        }
     }
 }
