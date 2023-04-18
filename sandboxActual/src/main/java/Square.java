1c79b78ca7422cdad32721e55d1f388f0efda1aa
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-04-18 16:46:21 +0300

    Sandbox was recreated again

---
+++ sandboxActual/src/main/java/Square.java
@@ -0,0 +1,12 @@
+public class Square {
+
+    public double l;
+
+    public Square (double len) {
+        this.l = len;
+    }
+    public double area() {
+        return this.l * this.l;
+    }
+
+}
